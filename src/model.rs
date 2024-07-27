bb7e9db30250026be195d7bfba5510f12dc5cf1b
jk <jonaskonschak@gmx.de>
2024-07-27 03:59:10 +0200

    impement IFBlock

--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,4 @@
 use tch::{nn, Tensor};
-use tch::nn::{conv2d, conv_transpose2d, Module, Path, ConvConfig, ConvTransposeConfig};
 
 pub fn warp(ten_input: &Tensor, ten_flow: &Tensor) -> Tensor {
     let (batch_size, _, height, width) = (
@@ -29,37 +28,99 @@ pub fn warp(ten_input: &Tensor, ten_flow: &Tensor) -> Tensor {
     ten_input.grid_sampler(&grid, 0, 1, true)
 }
 
-fn if_block_conv0(vs: &Path, i:i64, c:i64){
-    let mut config = ConvConfig::default();
-    config.stride = 2;
-    config.padding = 1;
-    config.dilation = 1;
-    nn::seq()
-        .add(conv2d(vs, i, c / 2, 3, config))
-        .add(conv2d(vs, c/2, c, 3, config));
+#[derive(Debug)]
+struct IFBlock {
+    conv0: nn::Sequential,
+    convblock: nn::Sequential,
+    lastconv: nn::ConvTranspose2D,
 }
 
-fn if_block_conv1(vs: &Path, c:i64){
-    let mut config = ConvConfig::default();
-    config.stride = 1;
-    config.padding = 1;
-    config.dilation = 1;
-    nn::seq()
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config))
-        .add(conv2d(vs, c, c, 3, config));
+impl IFBlock {
+    fn new(vs: &nn::Path, i: i64, c: i64) -> IFBlock {
+        let conv0_config = nn::ConvConfig {
+            stride: 2,
+            padding: 1,
+            dilation: 1,
+            ..Default::default()
+        };
+
+        let convblock_config = nn::ConvConfig {
+            stride: 1,
+            padding: 1,
+            dilation: 1,
+            ..Default::default()
+        };
+
+        let lastconv_config = nn::ConvTransposeConfig {
+            stride: 1,
+            padding: 1,
+            dilation: 1,
+            ..Default::default()
+        };
+
+        let prelu0_0_weight = vs.ones("prelu0_0_weight", &[c / 2]);
+        let prelu0_1_weight = vs.ones("prelu0_1_weight", &[c / 2]);
+
+        let prelu1_0_weight = vs.ones("prelu1_0_weight", &[c]);
+        let prelu1_1_weight = vs.ones("prelu1_1_weight", &[c]);
+        let prelu1_2_weight = vs.ones("prelu1_2_weight", &[c]);
+        let prelu1_3_weight = vs.ones("prelu1_3_weight", &[c]);
+        let prelu1_4_weight = vs.ones("prelu1_4_weight", &[c]);
+        let prelu1_5_weight = vs.ones("prelu1_5_weight", &[c]);
+        let prelu1_6_weight = vs.ones("prelu1_6_weight", &[c]);
+        let prelu1_7_weight = vs.ones("prelu1_7_weight", &[c]);
+
+        let conv0 = nn::seq()
+            .add(nn::conv2d(vs, i, c / 2, 3, conv0_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu0_0_weight))
+            .add(nn::conv2d(vs, c / 2, c, 3, conv0_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu0_1_weight));
+
+        let convblock = nn::seq()
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_0_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_1_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_2_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_3_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_4_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_5_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_6_weight))
+            .add(nn::conv2d(vs, c, c, 3, convblock_config))
+            .add_fn(move |xs: &Tensor| xs.prelu(&prelu1_7_weight));
+
+        let lastconv = nn::conv_transpose2d(vs, c, 5, 4, lastconv_config);
+
+        IFBlock {
+            conv0,
+            convblock,
+            lastconv,
+        }
+    }
+    fn prepare_args_flow(&self, xs: &Tensor, flow: &Tensor, scale: f64) -> Tensor {
+        let xs = xs.upsample_bilinear2d(&[xs.size()[2] / scale as i64, xs.size()[3] / scale as i64], false, None, None);
+        let flow = flow.upsample_bilinear2d(&[flow.size()[2] / scale as i64, flow.size()[3] / scale as i64], false, None, None) / scale;
+        Tensor::cat(&[xs, flow], 1)
+    }
+    fn prepare_args(&self, xs: &Tensor, scale: f64) -> Tensor {
+        xs.upsample_bilinear2d(&[xs.size()[2] / scale as i64, xs.size()[3] / scale as i64], false, None, None)
+    }
+    fn result_to_flow_mask(&self, xs: &Tensor) -> (Tensor, Tensor){
+        (xs.narrow(1, 0, 4)*2.0, xs.narrow(1, 4, 1))
+    }
 }
 
-fn if_block_conv2(vs: &Path,  c:i64){
-    let mut config = ConvTransposeConfig::default();
-    config.stride = 2;
-    config.padding = 1;
-    //nn::seq()
-    //    .add(conv_transpose2d(vs, c, 5, 4, config))
-    //    .add_fn(|x| x.prelu());
+impl nn::Module for IFBlock {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        let mut xs = xs.apply(&self.conv0);
+        xs = xs.apply(&self.convblock) + xs;
+        xs = xs.apply(&self.lastconv);
+        xs = xs.upsample_bilinear2d(&[xs.size()[2] * 2, xs.size()[3] * 2], false, None, None);
+        xs
+    }
 }
